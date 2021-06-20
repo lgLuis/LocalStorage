5ecd4c77baefe7bc0d1ebd636ebacc2b4405bdcc
lgLuis <luis_unlz@yahoo.com.ar>
2021-06-20 17:52:38 -0300

    js

--- JS/app.js
+++ JS/app.js
@@ -17,6 +17,29 @@ function eventListeners(){
 //FUNCIONES
 function agregarMensaje(e){
     e.preventDefault();
+    //TextArea Donde el usuario escribe:
+    const mens =document.querySelector("#mensaje").value;
+    console.log(mens);
+    //Validación
+    if(mens === ""){
+        mostrarError("El mensaje: No puede ir vacio")
+        return; //Evita que se sigan ejecutando más líneas de cód.
+    }
+}
 
-    console.log("agregando mensaje")
+//Mostrar mensaje de error
+
+function mostrarError(error){
+    const mensajeError = document.createElement("p");
+    mensajeError.textContent=error;
+    mensajeError.classList.add("error");
+    //Insertarlo en el contenido
+    const contenido=document.querySelector("#contenido");
+    contenido.appendChild(mensajeError);
+
+    //Eliminar mensaje error luego de  3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+
 }
