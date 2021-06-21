2fbf5926c9ab1dde485fbbc0bac8893950a545af
lgLuis <luis_unlz@yahoo.com.ar>
2021-06-21 16:15:27 -0300

    js terminado

--- JS/app.js
+++ JS/app.js
@@ -5,11 +5,19 @@ let mensajes=[];
 
 //EVENT LISTENERS
 eventListeners();
-
 function eventListeners(){
+    //cuando el usuario agrega un nuevo mensaje
     formulario.addEventListener("submit", agregarMensaje);
 
+    //cuando el documento esta listo
+    document.addEventListener("DOMContentLoaded",()=>{
+        mensajes=JSON.parse(localStorage.getItem("mens"))|| [];
+
+        console.log(mens);
+        crearHTML();
+    });
 }
+
 
 
 //FUNCIONES
@@ -32,14 +40,15 @@ function agregarMensaje(e){
     }
 
     //Agregar los mensajes al Arreglo
-    mensajes =[...mensajes, mensajeObj];
+    mensajes =[... mensajes, mensajeObj];
     //Creamos el html
     crearHTML();
-
+    //Reiniciar el formularo
+    formulario.reset();
 }
 
-//Mostrar mensaje de error
 
+//Mostrar mensaje de error
 function mostrarError(error){
     const mensajeError = document.createElement("p");
     mensajeError.textContent=error;
@@ -60,15 +69,42 @@ function crearHTML(){
     limpiarHTML();
     if(mensajes.length>0){
         mensajes.forEach(mens=>{
+            //Agregar un boton de eliminar
+            const btbEliminar =document.createElement("a");
+            btbEliminar.classList.add("borrar-mensaje");
+            btbEliminar.innerText= "X"
+            //Añadir la función de elimnar
+            btbEliminar.onclick=()=>{
+                borrarMensaje(mens.id);
+
+            };
+
             //Crear el HTML
             const li=document.createElement("li");
             //Agredo el texto
             li.innerText = mens.mens;
+            //Asignar el boton
+            li.appendChild(btbEliminar);
             //Insertando en el HTML
             listaMensajes.appendChild(li);
         });
     }
+    sincronizarStorage();
 }
+
+//Agrega los mensajes actuales al Local Storage
+function sincronizarStorage(){
+    localStorage.setItem("mensajes", JSON.stringify(mensajes));
+}
+
+//Eliminar un mensaje
+function borrarMensaje(id) {
+    // console.log("borrando...", id);
+    mensajes = mensajes.filter(mensajes => mensajes.id !== id);
+    // console.log(mensajes);
+    crearHTML();
+}
+
 
 //Limpiar el HTML
 function limpiarHTML(){
